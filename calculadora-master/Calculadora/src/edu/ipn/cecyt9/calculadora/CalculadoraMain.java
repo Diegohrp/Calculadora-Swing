b11c915e0b6f6925a6b07d5d65d47565f25b25f9
Diegohrp <30833448+Diegohrp@users.noreply.github.com>
2017-08-15 08:38:21 -0500

    Add files via upload
    Añadimos 5 operaciones más a la calculadora

---
+++ calculadora-master/Calculadora/src/edu/ipn/cecyt9/calculadora/CalculadoraMain.java
@@ -0,0 +1,23 @@
+
+package edu.ipn.cecyt9.calculadora;
+
+/**
+ * Ejercicio: Calculadora basica 
+ * Implementar la Interfaz de usuario 
+ * Implementar los metodos siguientes a través de los listeners adecuados: 
+ * - Sumar 
+ * - Restar 
+ * - Multiplicar 
+ * - Dividir
+ * 
+ *@author:  emmanuel
+ * Modificado por: Hernandez Velazquez Angel y Herrera Prado Diego
+ * @version:  6.0 
+ * @date: 06-09-2015 
+ */
+public class CalculadoraMain {
+ public static void main(String[] args) {
+		Calculadora calculadora = new Calculadora();
+		calculadora.setVisible(true);
+	}   
+}
